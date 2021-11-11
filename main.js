014a9ad46132bbd584007eec43e343c372861517
aahelexe <78219629+aahelexe@users.noreply.github.com>
2021-11-11 20:56:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90});
+camera=document.getElementById("camera");
+Webcam.attach('camera');
+function takeSnapshot()
+{
+    Webcam.snap(function(data_uri){
+        snapshot= '<img id="captured_image" src="'+data_uri+'"/>';
+        document.getElementById("result").innerHTML=snapshot;
+    });
+}
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/6pNjx6PiZ/model.js", modelReady);
+function modelReady(){
+    console.log("Model Ready!");
+}
+
